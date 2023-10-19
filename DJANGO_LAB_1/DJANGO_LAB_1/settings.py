645272a1ea93a314eac8bac06e06f9816d75f897
sasha-mykhailk <mykhailyk.oleksandr@chnu.edu.ua>
2023-10-20 01:14:14 +0300

    Second laba

--- DJANGO_LAB_1/DJANGO_LAB_1/settings.py
+++ DJANGO_LAB_1/DJANGO_LAB_1/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_blog'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -73,12 +75,26 @@ WSGI_APPLICATION = 'DJANGO_LAB_1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DJ_db',
+        'USER': 'xandr',
+        'PASSWORD': '1234567890',
+        'HOST': 'localhost',  # Може знадобитися вказати хост, якщо база даних розташована на тому ж сервері
+        # 'PORT': '', 
     }
 }
+
+ 
 
 
 # Password validation
