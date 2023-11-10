e23df0e20fbf839d5deac6155c1f39dc5406097b
sasha-mykhailk <mykhailyk.oleksandr@chnu.edu.ua>
2023-11-10 22:13:47 +0200

    Second commit for_sure

--- DJANGO_LAB_1/DJANGO_LAB_1/settings.py
+++ DJANGO_LAB_1/DJANGO_LAB_1/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
 }
 
  
-
+#  rthwrthbrwthgjeanrjkgggggggggggggggggkeberbkkerwrthwrthwrh 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
